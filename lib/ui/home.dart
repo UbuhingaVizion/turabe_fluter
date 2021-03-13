a4b503c396a419f3178e7b0a925d3faf049c347e
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2021-03-13 11:17:53 +0200

    Added bizcard

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,4 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class BizCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BizCard"),
+        centerTitle: true,
+        backgroundColor: Colors.black12,
+      ),
+      backgroundColor: Colors.grey,
+      body: Container(
+        alignment: Alignment.center,
+        child: Stack(
+          alignment: Alignment.topCenter,
+          children: [_getCard(), _getAvatar()],
+        ),
+      ),
+    );
+  }
+
+  Container _getCard() {
+    return Container(
+      width: 350,
+      height: 200,
+      margin: EdgeInsets.all(50.0),
+      decoration: BoxDecoration(
+          color: Colors.greenAccent, borderRadius: BorderRadius.circular(14.5)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Allan Stockman",
+            style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 23.5,
+                fontStyle: FontStyle.italic),
+          ),
+          Text(
+            "Learning Flutter",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Icon(Icons.person_outline), Text("Twitter : @iMitwe")],
+          )
+        ],
+      ),
+    );
+  }
+
+  Container _getAvatar() {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(50.0)),
+          border: Border.all(color: Colors.redAccent, width: 1.2),
+          image: DecorationImage(
+              image: NetworkImage("https://picsum.photos/300"),
+              fit: BoxFit.cover)),
+    );
+  }
+}
 
 class ScaffoldExample extends StatelessWidget {
   _tapButton() {
