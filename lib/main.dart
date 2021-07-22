06ed49211932acfb957da269ef27447c5c2edf8e
albi488 <albintabara@gmail.com>
2021-07-22 02:19:15 +0200

    theme

--- lib/main.dart
+++ lib/main.dart
@@ -5,5 +5,19 @@ import 'ui/home.dart';
 // void main() => runApp(ScaffoldExample());
 
 void main() => runApp(new MaterialApp(
+  theme: ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: Colors.deepOrange.shade300,
+    textTheme: TextTheme(
+    headline1: TextStyle(
+      fontSize: 34,
+      fontWeight: FontWeight.bold,
+    ),
+    bodyText2: TextStyle(
+      fontSize: 16.9,
+      color: Colors.redAccent,
+    ),
+    ),
+  ),
       home: MovieListView(),
     ));
